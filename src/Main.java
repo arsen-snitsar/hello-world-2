ba0d4cb53f75bb1cc0595d7244e8d046e2e73fc1
arsen-snitsar <vstidm@proton.me>
2023-08-22 15:58:25 +0300

    222

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("My name is Arsen. Welcome to my first Java program.");
         System.out.println("2 + 2 = 4");
+        System.out.println("3 + 3 = 6");
+        /*System.out.println("The sun shines and the wind blows.");
+        System.out.println("The last line of the program!");*/
     }
 }
